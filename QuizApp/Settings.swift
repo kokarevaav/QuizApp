430d30924e39c553a176db2bce79976f8634d771
Alexandra Kokareva <mrs.kokareva@gmail.com>
2022-06-15 22:13:00 +0300

    Revert "project structure fix"
    This reverts commit 3e6afd7bd9ce424278f664ed0299a3eae2c21245.
    
    clear
    clear

--- QuizApp/Settings.swift
+++ QuizApp/Settings.swift
@@ -37,7 +37,7 @@ struct Difficulties {
 }
 
 struct AmountOfQuestions {
-    static let amountDictionary: [String : String] = ["easy" : "15",
+    static let amountDictionary: [String : String] = ["easy" : "3",
                                                       "medium" : "10",
                                                       "hard" : "10"]
 }
